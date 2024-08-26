f7cb727820ca792a208455cd368e07873e19f75a
Alfie Hillman <ahillman02@hotmail.co.uk>
2024-08-26 15:18:13 +0100

    Changed colour input box  into combobox to avoid erroneous inputs

--- AddItemWindow.xaml.cs
+++ AddItemWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,19 @@ namespace Kanban
     public partial class AddItemWindow : Window
     {
         public static MainWindow.KanbanItem? CreatedItem { get; set; }
+        public ObservableCollection<string>? Items { get; set; }
         public AddItemWindow()
         {
             InitializeComponent();
             CreatedItem = null;
+            Items = new ObservableCollection<string>();
+            Items.Add("LightGray");
+            Items.Add("Red");
+            Items.Add("Blue");
+            Items.Add("Green");
+            Items.Add("Yellow");
+            colourbox.ItemsSource = Items;
+            colourbox.SelectedItem = "LightGray";
         }
 
         public void AddClick(object sender, RoutedEventArgs e)
